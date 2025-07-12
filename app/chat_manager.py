d95d5bbbd61fe0ee4519c3b9dbb99a450f374f3c
Mohanshi <jainmohanshi@gmail.com>
2025-07-12 18:12:39 +0530

    editing chat_manager.py

--- app/chat_manager.py
+++ app/chat_manager.py
@@ -4,8 +4,8 @@ from app.models import Message
 
 class ChatManager:
     def __init__(self):
-        self.connections = {}
-        self.redis = RedisPubSub()
+        self.connections = {}   #stores active WebSocket connections for each room
+        self.redis = RedisPubSub()  #to use Redis pub/sub for broadcasting
 
     async def connect(self, websocket, room):
         await websocket.accept()
@@ -13,15 +13,15 @@ class ChatManager:
         await self.redis.subscribe(room, lambda msg: self.broadcast(room, msg))
 
     async def handle_message(self, room, message):
-        await self.redis.publish(room, message)
+        await self.redis.publish(room, message) #publishes msg on redis so all receivers can recieve it
         db = SessionLocal()
         db.add(Message(room=room, content=message))
-        db.commit()
+        db.commit()                             #saves msg to db
         db.close()
 
-    async def broadcast(self, room, message):
+    async def broadcast(self, room, message):       #sends msg to all websocket clients in that room
         for ws in self.connections.get(room, []):
             await ws.send_text(message)
 
-    async def disconnect(self, websocket, room):
+    async def disconnect(self, websocket, room):    #removes that websocket(user) from room
         self.connections[room].remove(websocket)
