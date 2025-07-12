51621f88bb3ba7c949436e86b244fd6b64a729fd
Mohanshi <jainmohanshi@gmail.com>
2025-07-12 18:09:52 +0530

    editing redis_pubsub.py

--- app/redis_pubsub.py
+++ app/redis_pubsub.py
@@ -1,11 +1,11 @@
-import redis.asyncio as redis
-import asyncio
+import redis.asyncio as redis   #assume redis to be netflix to understand better
+import asyncio      #asynchronous I/O
 
 class RedisPubSub:
     def __init__(self):
-        self.redis = redis.Redis(host="redis", port=6379, decode_responses=True)
+        self.redis = redis.Redis(host="redis", port=6379, decode_responses=True)    #connects to Redis container (host = redis from Docker)
 
-    async def publish(self, room: str, message: str):
+    async def publish(self, room: str, message: str):       #publishes a msg to a redis channel(room)
         await self.redis.publish(room, message)
 
     async def subscribe(self, room: str, callback):
